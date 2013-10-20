802f8396347494985dd4fecf0fd61aee3de2c243
Rahul Somasunderam <rahul.som@gmail.com>
2013-10-19 22:13:56 -0700

    Fixed command

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ "$TRAVIS_PULL_REQUEST" = false ]; then
   if [ "$TRAVIS_BRANCH" = "master" ]; then
   	./gradlew uberjar
   else
-    python addServer.py && mvn deploy --settings ~/.m2/mySettings.xml
+    ./gradlew uploadArchives
   fi
 else
   ./gradlew uberjar
